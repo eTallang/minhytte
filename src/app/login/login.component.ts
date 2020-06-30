6023cac3de870c10cdbe3df4d7736745beb916a3
Erik Tallang <e.tallang@gmail.com>
2020-06-30 13:45:24 +0200

    fix(login): bad response to user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'mh-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
+  private unsubscriber = new Subject<void>();
   fg = new FormGroup({
     email: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required)
   });
+  error = '';
+  signingIn = false;
 
   constructor(private authService: AngularFireAuth, private router: Router) {}
 
+  ngOnInit(): void {
+    this.fg.statusChanges.pipe(takeUntil(this.unsubscriber)).subscribe(() => {
+      if (this.fg.touched && this.fg.invalid) {
+        this.error = 'Vennligst skriv inn e-post og passord.';
+      } else {
+        this.error = '';
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+  }
+
   signIn(): void {
+    this.signingIn = true;
     const values = this.fg.getRawValue();
     this.authService.signInWithEmailAndPassword(values.email, values.password).then(() => {
       this.router.navigate(['']);
+      this.signingIn = false;
+    }, err => {
+      this.error = err.message;
+      this.signingIn = false;
     });
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,11 +5,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { IconModule } from '../shared/icon/icon.module';
+import { A11yModule } from '@angular/cdk/a11y';
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
+    A11yModule,
     CommonModule,
     LoginRoutingModule,
     ReactiveFormsModule,
