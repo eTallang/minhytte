aaf3c027457fee33d3966c7fdce83af25970ac6b
Erik Tallang <e.tallang@gmail.com>
2020-07-10 00:27:45 +0200

    feat(icon): support any color

--- src/app/shared/icon/icon.component.ts
+++ src/app/shared/icon/icon.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'mh-icon',
   templateUrl: './icon.component.html',
   styleUrls: ['./icon.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class IconComponent {
   @Input() size: 'small' | 'medium' | 'large' = 'large';
-  @Input() theme: 'light' | 'dark' = 'dark';
   @Input() name = '';
 }
