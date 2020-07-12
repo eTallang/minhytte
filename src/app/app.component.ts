e7f3ceb00ae1a13f0d0e0c455eff0f680de0da8a
Erik Tallang <e.tallang@gmail.com>
2020-07-12 23:16:38 +0200

    feat(app): check for app updates on routing events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil, filter } from 'rxjs/operators';
 
-import { SwService } from './core/sw.service';
+import { AlertService } from './shared/alert/alert.service';
 
 @Component({
   selector: 'mh-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(private swService: SwService) {}
+export class AppComponent implements OnInit, OnDestroy {
+  private unsubscriber = new Subject<void>();
+
+  constructor(private swUpdate: SwUpdate, private alert: AlertService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkForUpdates();
+    this.router.events
+      .pipe(
+        filter((e) => e instanceof NavigationEnd),
+        takeUntil(this.unsubscriber)
+      )
+      .subscribe(() => {
+        this.swUpdate.checkForUpdate();
+      });
+  }
+
+  private checkForUpdates() {
+    this.swUpdate.available.pipe(takeUntil(this.unsubscriber)).subscribe((d) => {
+      if (d.current !== d.available) {
+        this.alert
+          .open(
+            'Hei du!',
+            'En ny oppdatering av appen er tilgjengelig. Trykk på knappen nedenfor for å få siste versjon.',
+            {
+              action: 'Oppdater',
+              closeable: true
+            }
+          )
+          .afterClosed()
+          .subscribe((refresh) => {
+            if (refresh) {
+              location.reload();
+            }
+          });
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { NgModule } from '@angular/core';
@@ -7,8 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { ShellModule } from './shell/shell.module';
-import { IconModule } from './shared/icon/icon.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { AlertModule } from './shared';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +18,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     ShellModule,
-    IconModule,
+    AlertModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   bootstrap: [AppComponent]
